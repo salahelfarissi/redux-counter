ff3aeb2edf26ca9e03cd9adae77ad2baa81f65a1
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-22 18:49:42 +0200

    useMemo to memorize value

--- src/useActions.js
+++ src/useActions.js
@@ -1,7 +1,8 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { useMemo } from 'react';
 
 export const useActions = (actions) => {
     const dispatch = useDispatch();
-    return bindActionCreators(actions, dispatch);
+    return useMemo(() => bindActionCreators(actions, dispatch), [actions, dispatch]);
 };
