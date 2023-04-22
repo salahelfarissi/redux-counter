27d22945eaaa5efe230673b1302746f469a7bf2e
salahelfarissi <salaheddine.elfarissi@gmail.com>
2023-04-22 10:23:13 +0200

    Wrap App with Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import { store } from './store';
 
 import './index.scss';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>);
